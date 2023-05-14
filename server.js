ee156a915dd5023ccb2a44209fb8bce96b71f235
Peter Cardillo <pcardillo@streamingturtles.com>
2023-05-14 18:05:45 -0400

    updated a comment

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+// todo - try this to see what happens 
+// playing with server block logic - try this
+// const PORT = process.env.PORT || 3001 || 80
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('hello world, here come the streamingturtles - finally with 2vaapom');
 });
 
 app.listen(PORT, () => {
